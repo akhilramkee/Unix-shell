5270ad7d7d16a023087057cfa77fb323df30d6ae
Akhilesh <akhilramkee123@gmail.com>
2019-08-14 16:41:09 +0530

    Added data

--- scrape.sh
+++ scrape.sh
@@ -1,10 +1,20 @@
 echo "Enter the Mangastream site address:"
 read sitename
+i=1
+adder="/"
 header="https:"
-Site="$header$sitename"
+Site="$header$sitename$adder$i"
 echo "$Site"
+curl $Site > page.html
+pagecount=`tac page.html|grep -B 50 -m1 "<ul class=\"dropdown-menu\">"|awk '{print $3}'|cut -d '<' -f 1|grep -v '^[[:alpha:]]'|grep '[[:digit:]]'|head -1`
+echo "Pagecount:$pagecount"
+while [ $i -le $pagecount ]
+do
+Site="$header$sitename$adder$i"
 curl $Site>page.html
 image=`cat page.html| grep "img" |cut -d '>' -f 2| cut -d '<' -f 2| awk -F '=' '{print $3}'|cut -d '"' -f 2`
 imageAddr="$header$image"
 echo "$imageAddr"
-curl $imageAddr > imagex.jpg
+curl $imageAddr > "image$i".jpg
+i=$((i+1))
+done
