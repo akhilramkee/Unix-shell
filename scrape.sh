daa6843fbc059c431cced8871000adf38e024056
Akhilesh <akhilramkee123@gmail.com>
2019-08-15 00:01:22 +0530

    Updated scraping pdf from Mangastream

--- scrape.sh
+++ scrape.sh
@@ -6,8 +6,11 @@ header="https:"
 Site="$header$sitename$adder$i"
 echo "$Site"
 curl $Site > page.html
-pagecount=`tac page.html|grep -B 50 -m1 "<ul class=\"dropdown-menu\">"|awk '{print $3}'|cut -d '<' -f 1|grep -v '^[[:alpha:]]'|grep '[[:digit:]]'|head -1`
+pagecount=`cat page.html|grep "Last Page"|awk -F '<|>' '{print $5}'|awk -F '(' '{print $2}'|cut -d ')' -f 1`
 echo "Pagecount:$pagecount"
+
+
+#Provides you with items required
 while [ $i -le $pagecount ]
 do
 Site="$header$sitename$adder$i"
@@ -18,3 +21,10 @@ echo "$imageAddr"
 curl $imageAddr > "image$i".jpg
 i=$((i+1))
 done
+
+Filename=`echo "$sitename"|awk -F '/' '{print $5"_"$6}'`
+#After changing the permissions in /etc/ImageMagick-6/policy.xml
+convert $(ls -1 image*.jpg| sort -V) "$Filename".pdf
+
+#Save space
+rm image*.jpg
